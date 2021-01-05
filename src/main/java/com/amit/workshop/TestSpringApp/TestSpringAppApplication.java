9773200f2fb0d0a07b3796b17dd9261d84974659
student <kulshrestha.amit@gmail.com>
2021-01-05 10:19:08 +0000

    Adding Springboot Project

---
+++ src/main/java/com/amit/workshop/TestSpringApp/TestSpringAppApplication.java
@@ -0,0 +1,13 @@
+package com.amit.workshop.TestSpringApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSpringAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestSpringAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amit/workshop/TestSpringApp/TestSpringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amit.workshop.TestSpringApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSpringAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
